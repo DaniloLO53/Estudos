fa575505cc40b50d50a9faf1b2366e6f63f71ef0
DaniloLO53 <daniloleaodev@gmail.com>
2025-01-14 14:48:47 -0300

    day  11

--- DSA/Dinesh Varyani/course/src/main/java/arrays/Main.java
+++ DSA/Dinesh Varyani/course/src/main/java/arrays/Main.java
@@ -1,4 +1,15 @@
 package arrays;
 
 public class Main {
+    public static void printArr(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.printf("Arr[%d]: %d\n", i, arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 6, 7, 8, 10};
+        printArr(RemoveEven.remove(arr1));
+//        printArr(arr1);
+    }
 }
--- DSA/Dinesh Varyani/course/src/main/java/arrays/RemoveEven.java
+++ DSA/Dinesh Varyani/course/src/main/java/arrays/RemoveEven.java
@@ -1,13 +1,7 @@
 package arrays;
 
-public class OneDimensionalArray {
-    public static void printArr(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            System.out.printf("Arr[%d]: %d\n", i, arr[i]);
-        }
-    }
-
-    public static int[] removeEven(int[] arr) {
+public class RemoveEven {
+    public static int[] remove(int[] arr) {
         int newArrLength = 0;
         for (int n: arr) {
             if(n % 2 != 0) {
@@ -25,10 +19,5 @@ public class OneDimensionalArray {
         }
 
         return newArr;
-    }
-    public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5, 6, 7, 8, 10};
-        printArr(removeEven(arr1));
-//        printArr(arr1);
     }
 }
