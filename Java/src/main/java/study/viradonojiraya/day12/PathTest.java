d794ba82f1b0cd2a1d79a25a0b42ea5a300a4e66
DaniloLO53 <daniloleaodev@gmail.com>
2025-01-15 14:09:39 -0300

    update utility classes

--- Java/src/main/java/study/viradonojiraya/day12/PathTest.java
+++ Java/src/main/java/study/viradonojiraya/day12/PathTest.java
@@ -1,5 +1,19 @@
 package study.viradonojiraya.day12;
 
-public class PathTest {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class PathTest {
+    public static void main(String[] args) {
+        Path dirPath = Paths.get("tests");
+
+        try {
+            Path dir = Files.createDirectory(dirPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
