185660581e2a135baf0904bdc5a55e01355686bb
DaniloLO53 <daniloleaodev@gmail.com>
2025-01-20 18:42:01 -0300

    day14

---
+++ Java/src/main/java/study/viradonojiraya/day14/BinarySearchTest.java
@@ -0,0 +1,20 @@
+package study.viradonojiraya.day14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BinarySearchTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(0);
+        numbers.add(4);
+        numbers.add(3);
+
+        Collections.sort(numbers);
+
+        System.out.println(Collections.binarySearch(numbers, 1)); //(-(insertion point) - 1)
+    }
+}
