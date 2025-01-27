10b3a4699e1f39b4be005290849813d07624461d
DaniloLO53 <daniloleaodev@gmail.com>
2025-01-27 10:03:04 -0300

    patterns - dsa

--- algorithms/src/main/java/domain/arrays/patterns/PatternsTest.java
+++ algorithms/src/main/java/domain/arrays/patterns/PatternsTest.java
@@ -1,4 +1,62 @@
 package domain.arrays.patterns;
 
 public class PatternsTest {
+    static void pattern1(int n) {
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(j+1);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(i+1);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    static void pattern6(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern6(5);
+    }
+
 }
