4fef8a123c0a451dc7eb210dba8abd5023009e01
DaniloLO53 <daniloleaodev@gmail.com>
2025-05-19 09:07:12 -0300

    up

--- Java/src/main/java/study/random/OOP/Enemy.java
+++ Java/src/main/java/study/random/OOP/Enemy.java
@@ -1,55 +1,86 @@
 package study.random.OOP;
 
 public class Enemy {
-    private float attackScore;
-    private float defenseScore;
-    private String name;
-    private String[] weaknesses;
+    private float health = 5000;
+    private char mode;
+    private static float attackScore;
+    private static float defenseScore;
+    private static Item[] items;
+    private EnemyHorde horde;
 
-    {
-
-    }
-
-    public Enemy(float attackScore, float defenseScore) {
-        this.attackScore = attackScore;
-        this.defenseScore = defenseScore;
-    }
-
-    public Enemy(float attackScore, float defenseScore, String name) {
-        this(attackScore, defenseScore);
-        this.name = name;
+    static {
+        attackScore = EnemySavedStatus.getAttackScore();
+        defenseScore = EnemySavedStatus.getDefenseScore();
     }
 
     public Enemy() {
 
     }
 
-    public void showInfo(float attackScore) {
-        System.out.printf("""
-                Attack score: %s
-                """, attackScore);
+    public Enemy(char mode) {
+        this.mode = mode;
     }
 
-    public void showInfo(float attackScore, float defenseScore) {
-        System.out.printf("""
-                Attack score: %s
-                Defense score: %s
-                """, attackScore, defenseScore);
+    public Enemy(char mode, Item[] items) {
+        this(mode);
+        Enemy.items = items;
     }
 
-    public float getAttackScore() {
+    public void showItems() {
+        if (items != null) {
+            for (Item item : items) {
+                System.out.println(item.getName());
+            }
+        } else {
+            System.out.println("NONE");
+        }
+    }
+
+    public EnemyHorde getHorde() {
+        return horde;
+    }
+
+    public void setHorde(EnemyHorde horde) {
+        this.horde = horde;
+    }
+
+    public static Item[] getItems() {
+        return items;
+    }
+
+    public static void setItems(Item[] items) {
+        Enemy.items = items;
+    }
+
+    public char getMode() {
+        return mode;
+    }
+
+    public void setMode(char mode) {
+        this.mode = mode;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+    public static float getAttackScore() {
         return attackScore;
     }
 
-    public void setAttackScore(float attackScore) {
-        this.attackScore = attackScore;
+    public static void setAttackScore(float attackScore) {
+        Enemy.attackScore = attackScore;
     }
 
-    public float getDefenseScore() {
+    public static float getDefenseScore() {
         return defenseScore;
     }
 
-    public void setDefenseScore(float defenseScore) {
-        this.defenseScore = defenseScore;
+    public static void setDefenseScore(float defenseScore) {
+        Enemy.defenseScore = defenseScore;
     }
 }
--- Java/src/main/java/study/random/OOP/Main.java
+++ Java/src/main/java/study/random/OOP/Main.java
@@ -2,9 +2,13 @@ package study.random.OOP;
 
 public class Main {
     public static void main(String[] args) {
-        Enemy enemy = new Enemy();
+        Item item1 = new Item("Sword");
+        Item item2 = new Item("Shield");
+        Enemy enemy1 = new Enemy('A', new Item[]{item1, item2});
+        Enemy enemy2 = new Enemy('B', new Item[]{item1});
 
-        enemy.showInfo(1000);
-        enemy.showInfo(1000, 2000);
+        EnemyHorde enemyHorde = new EnemyHorde(enemy1, enemy2);
+
+        enemy1.showItems();
     }
 }
